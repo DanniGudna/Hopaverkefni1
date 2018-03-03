5748780f8e4388ed345ef4f301ded41a9fe15fd0
dingolfsson <dai5@hi.is>
2018-03-03 21:43:22 +0000

    Tables to be used

---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(64) UNIQUE NOT NULL,
+  passwd VARCHAR(255) NOT NULL,
+  fname VARCHAR(255) NOT NULL,
+  avatar TEXT
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    categoryName VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    isbn13 VARCHAR(13) UNIQUE NOT NULL,
+    author VARCHAR(64),
+    description TEXT,
+    categoryName VARCHAR(255) UNIQUE NOT NULL,
+    isbn10 VARCHAR(10) UNIQUE,
+    publishDate DATETIME,
+    pages INT,
+    lang VARCHAR(2),
+    FOREIGN KEY (categoryName) REFERENCES categories (categoryName)
+);
+
+CREATE TABLE readBooks (
+    id SERIAL PRIMARY KEY,
+    userID serial NOT NULL,
+    bookID serial NOT NULL,
+    rating INT NOT NULL,
+    review TEXT,
+    FOREIGN KEY (userID) REFERENCES users (id),
+    FOREIGN KEY (bookID) REFERENCES books (id)
+);
